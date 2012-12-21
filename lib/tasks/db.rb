e4aa163f9f6c921151339a693f8147e5d936888a
assetsmc <cocozz@gmail.com>
2012-12-21 14:11:08 +0100

    add db task file

---
+++ lib/tasks/db.rb
@@ -0,0 +1,7 @@
+task :cleandb  => [:environment] do
+  Historial.all.each { |i| i.destroy }
+  Histgen.all.each { |i| i.destroy }
+  Histimp.all.each { |i| i.destroy }
+  Histpag.all.each { |i| i.destroy }
+  Moviment.all.each { |i| i.destroy }
+end
